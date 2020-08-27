24e54698934ab8a40ac677ba2cdab06b84e23a6c
Neil Chauhan <neilchauhan2@gmail.com>
2020-08-27 14:18:19 +0530

    Cleanup and file setup

---
+++ src/components/Nominations.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nominations = () => {
+    return (
+        <div>
+            <h1>Nominations</h1>
+        </div>
+    );
+};
+
+export default Nominations;
---
+++ src/components/searchResult/ResultItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResultItem = () => {
+    return (
+        <div>
+            <h1>Result Item</h1>
+        </div>
+    );
+};
+
+export default ResultItem;
